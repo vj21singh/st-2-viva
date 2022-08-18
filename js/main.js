b918d076557f312dde586c73439124b9d4a38511
Vijay <vijay1772.be20@chitkara.edu.in>
2022-08-18 13:42:03 +0530

    Image Slider Created

--- js/main.js
+++ js/main.js
@@ -13,3 +13,34 @@ const responsive = {
     }
 }
 
+$(document).ready(function () {
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /** click event on toggle menu */
+    $toggleCollapse.click(function () {
+        $nav.toggleClass('collapse');
+    })
+    // owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        dots: true,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
+
+    // click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+    // AOS Instance
+    AOS.init();
+
+});
